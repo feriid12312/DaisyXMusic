a7b82ccb399c0c3d44d9aedfd3e426213f3f9ec4
dadadadd1321 <87093609+feriid12312@users.noreply.github.com>
2021-08-09 20:51:10 +0400

    Update userbotjoin.py

--- DaisyXMusic/modules/userbotjoin.py
+++ DaisyXMusic/modules/userbotjoin.py
@@ -22,7 +22,7 @@ from DaisyXMusic.helpers.decorators import authorized_users_only, errors
 from DaisyXMusic.services.callsmusic.callsmusic import client as USER
 
 
-@Client.on_message(filters.command(["userjoin"]) & ~filters.private & ~filters.bot)
+@Client.on_message(filters.command(["userbotjoin"]) & ~filters.private & ~filters.bot)
 @authorized_users_only
 @errors
 async def addchannel(client, message):
