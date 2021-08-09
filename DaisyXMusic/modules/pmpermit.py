34653a5689feb84af9e6de20e3db2bfc6f80770a
dadadadd1321 <87093609+feriid12312@users.noreply.github.com>
2021-08-09 21:02:58 +0400

    Update pmpermit.py

--- DaisyXMusic/modules/pmpermit.py
+++ DaisyXMusic/modules/pmpermit.py
@@ -1,83 +1,16 @@
-# Daisyxmusic (Telegram bot project )
-# Copyright (C) 2021  Inukaasith
+# Daisyxmusic (Telegram bot proje )
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-from pyrogram import Client
-import asyncio
-from DaisyXMusic.config import SUDO_USERS, PMPERMIT
 from pyrogram import filters
 from pyrogram.types import Message
+
 from DaisyXMusic.services.callsmusic.callsmusic import client as USER
 
-PMSET =True
-pchats = []
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-    if PMPERMIT == "ENABLE":
-        if PMSET:
-            chat_id = message.chat.id
-            if chat_id in pchats:
-                return
-            await USER.send_message(
-                message.chat.id,
-                "Hi there, This is a music assistant service .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n ⚠️ Disclamer: If you are sending a message here it means admin will see your message and join chat\n    - Don't add this user to secret groups.\n   - Don't Share private info here\n\n",
-            )
-            return
-
-    
-
-@Client.on_message(filters.command(["/pmpermit"]))
-async def bye(client: Client, message: Message):
-    if message.from_user.id in SUDO_USERS:
-        global PMSET
-        text = message.text.split(" ", 1)
-        queryy = text[1]
-        if queryy == "on":
-            PMSET = True
-            await message.reply_text("Pmpermit turned on")
-            return
-        if queryy == "off":
-            PMSET = None
-            await message.reply_text("Pmpermit turned off")
-            return
-
-@USER.on_message(filters.text & filters.private & filters.me)        
-async def autopmPermiat(client: USER, message: Message):
-    chat_id = message.chat.id
-    if not chat_id in pchats:
-        pchats.append(chat_id)
-        await message.reply_text("Approoved to PM due to outgoing messages")
-        return
-    message.continue_propagation()    
-    
-@USER.on_message(filters.command("a", [".", ""]) & filters.me & filters.private)
-async def pmPermiat(client: USER, message: Message):
-    chat_id = message.chat.id
-    if not chat_id in pchats:
-        pchats.append(chat_id)
-        await message.reply_text("Approoved to PM")
-        return
-    message.continue_propagation()    
-    
-
-@USER.on_message(filters.command("da", [".", ""]) & filters.me & filters.private)
-async def rmpmPermiat(client: USER, message: Message):
-    chat_id = message.chat.id
-    if chat_id in pchats:
-        pchats.remove(chat_id)
-        await message.reply_text("Dispprooved to PM")
-        return
-    message.continue_propagation()    
+    await USER.send_message(
+        message.chat.id,
+        "Salam bu @BTCMusicBOT Assistant hesabıdır!\n\n👉 **BOT GRUPA QATILMAZSA BOTU ƏL İLƏ GRUPA QATIN**\n\n**QAYDALAR**❗️\n- Hesaba gizli məlumatıarınızı yazmayın\n- Hesabı kontakta əlavə etməyin\n- Hesabı gizli gruplarınıza əlavə etməyin\n\nUnutmayın ki admin assistant hesabına yazılan mesaları görur\nQaydalara əməl etmədiyiniz halda hər hansı bir prablem olarsa **GROUP MUZİK BOT** admini heçbir məsuliyət daşımır❗️ \n\n**Bota bağlı köməyə ehtiyacınız olarsa dəstək grupuna və ya bot sahibindən kömək alın\n\nDəstək grupu: @BTCSohbet\nBot Sahibi: @MenZroyam",
+    )
+    return
